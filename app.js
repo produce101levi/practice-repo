fecb1f79336019c65a16d05f065f6623b9be5ec2
Levi <Daz>
2025-04-15 15:16:43 -0600

    feat: modify app.get function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! WAITTTTTT DOES THIS ACTUALLY WORK?!!?')
 })
 
 app.listen(port, () => {
