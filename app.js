0c915a6226541e0d2ab60bf2b3de831d6e38c94b
Levi <Daz>
2025-04-15 15:36:48 -0600

    feat: add view with video

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const port = process.env.PORT || 3000;
+
+// Set the 'views' directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (like CSS, videos, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World! WAITTTTTT DOES THIS ACTUALLY WORK?!!?')
+    res.sendFile(path.join(__dirname, 'views', 'stanloona.html'));
 })
 
 app.listen(port, () => {
